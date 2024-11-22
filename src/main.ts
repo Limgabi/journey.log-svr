12602003fac1ba4aa53d711150e7975095cc4d69
gabi <70315572+Limgabi@users.noreply.github.com>
2024-11-22 21:49:27 +0900

    chore #5 : CORS 허용 설정

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [process.env.CLIENT_BASE_URL],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
   await app.listen(8080);
 }
 bootstrap();
